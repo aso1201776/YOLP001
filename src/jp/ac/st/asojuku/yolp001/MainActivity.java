e84c41d75d6be7bf6d8a371d6c30f42a6732ec49
student <student@PC111S10.fgakusei.local>
2014-07-25 15:10:04 +0900

    7/25

--- src/jp/ac/st/asojuku/yolp001/MainActivity.java
+++ src/jp/ac/st/asojuku/yolp001/MainActivity.java
@@ -9,8 +9,8 @@ import jp.co.yahoo.android.maps.routing.RouteOverlay.RouteOverlayListener;
 import jp.co.yahoo.android.maps.weather.WeatherOverlay;
 import jp.co.yahoo.android.maps.weather.WeatherOverlay.WeatherOverlayListener;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -20,6 +20,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
 
 
@@ -46,8 +48,6 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 	GeoPoint mStartPos;
 	//目的地
 	GeoPoint mGoalPos;
-	//プログレスダイアログ
-	ProgressDialog mProgDialog = null;
 	//距離表示用テキストビュー
 	TextView mDistLabel = null;
 	//メニュークリア
@@ -66,11 +66,11 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO 自動生成されたメソッド・スタブ
 		//メニュー選択処理
-		switch (item.getItemID()) {
+		switch (item.getItemId()) {
 			case MENUITEM_CLEAR:
 				//地図上からルートと距離表示をクリア
 				if(mMapView != null) {
-					mMapView.removeOverlay(mRouteOverlay);
+					mMapView.getOverlays().remove(mRouteOverlay);
 					mRouteOverlay = null;
 					if(mDistLabel != null) mDistLabel.setVisibility(View.INVISIBLE);
 				}
@@ -102,13 +102,43 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 			//今回の緯度経度を覚える
 			this.lastLatitude = latitude;
 			this.lastLongitube = longitube;
+
+			//ここからルート検索処理を追加
+			//LocationManagerで取得した位置にピンを立てる
+			mPinOverlay = new PinOverlay(PinOverlay.PIN_VIOLET);
+			mMapView.getOverlays().add(mPinOverlay);
+			mPinOverlay.addPoint(gp,null);
+
+			//LocationManagerで取得した位置をルート検索位置とします
+			mStartPos = gp;
 		}
 
 	}
 
+	/**
+	 * ルート検索処理が完了したときに反応するイベントリスナー
+	 * arg0 ルート検索結果を表示する表示レイヤー
+	 */
 	@Override
 	public boolean finishRouteSearch(RouteOverlay arg0) {
 		// TODO 自動生成されたメソッド・スタブ
+
+		//距離テキストビュー
+		if(mDistLabel != null){
+			mDistLabel.setVisibility(View.VISIBLE);
+		}else{
+			//初めてなら、newしてTextViewを画面に動的に作る
+			mDistLabel = new TextView(this);
+			this.addContentView((View)mDistLabel,
+					new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		}
+		//TextViewの文字サイズ、文字色、背景色を設定
+		mDistLabel.setTextSize(20);
+		mDistLabel.setTextColor(Color.argb(255, 255, 255, 255));
+		mDistLabel.setBackgroundColor(Color.argb(127, 0, 0, 0));
+
+		//距離を表示する文言を設定
+		mDistLabel.setText(String.format("距離　%.3fキロメートル",(arg0.getDistance()/1000)));
 		return false;
 	}
 
@@ -142,9 +172,54 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 
 	}
 
+	/**
+	 * 地図上を長押しした時に反応するイベントリスナー
+	 * arg1 MapView
+	 * arg2 ピン表示用の表示レイヤー
+	 * arg3 長押しした地図上の地点の緯度経度のGeoPoint
+	 */
 	@Override
 	public boolean onLongPress(MapView arg0, Object arg1, PinOverlay arg2, GeoPoint arg3) {
 		// TODO 自動生成されたメソッド・スタブ
+
+		//前回の0処理を停止
+		if(mRouteOverlay != null){
+			mRouteOverlay.cancel();
+		}
+		//目的地を設定
+		mGoalPos = arg3;
+
+		//長押しピンをクリア　onLongPressが発生すると自動敵にピンが追加されるので、ここで削除しておく
+		mMapView.getOverlays().remove(arg2);
+
+		//前回のRouteOverlayを地図から削除
+		mMapView.getOverlays().remove(mRouteOverlay);
+
+		//距離テキストビューを非表示
+		if(mDistLabel != null){
+			mDistLabel.setVisibility(View.INVISIBLE);
+		}
+		//RouteOverlay作成
+		//RouteOverlayはYOLPサーバにアクセスするので、再びアプリケーションIDを設定(地図表示用のMapViewに設定したのと同じでよい)
+		mRouteOverlay = new RouteOverlay(this,"dj0zaiZpPTdhZ1hERlB4QU01ViZzPWNvbnN1bWVyc2VjcmV0Jng9Mjg-");
+
+		//出発地ピン吹き出し設定
+		mRouteOverlay.setStartTitle("出発地");
+
+		//目的地ピン吹き出し設定
+		mRouteOverlay.setGoalTitle("目的地");
+
+		//出発地、目的地、移動手段を設定
+		mRouteOverlay.setRoutePos(mStartPos, mGoalPos, RouteOverlay.TRAFFIC_WALK);
+
+		//RouteOverlayListenerの設定
+		mRouteOverlay.setRouteOverlayListener(this);
+
+		//ルート検索を実行
+		mRouteOverlay.search();
+
+		//MapView弐ルート検索後のRouteOverlayを追加
+		mMapView.getOverlays().add(mRouteOverlay);
 		return false;
 	}
 
@@ -180,10 +255,6 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 		//現在位置
 		mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
 		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,0,this);
-		//プログレスダイアログを表示
-		mProgDialog = new ProgressDialog(this);
-		mProgDialog.setMessage("現在位置取得中");
-		mProgDialog.show();
 
 	}
 
@@ -243,12 +314,32 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 
 		//MapViewにWeatherOverlayを追加
 		mMapView.getOverlays().add(mWeatherOverlay);
+
+		//ここからルート検索処理を追加
+		//地図上で長押しイベントの発行を許可する
+		mMapView.setLongPress(true);
+
+		//MapTouchListenerを設定
+		mMapView.setMapTouchListener(this);
+
+		//LocationManagerで取得した位置にピンを立てる
+		mPinOverlay = new PinOverlay(PinOverlay.PIN_VIOLET);
+		mMapView.getOverlays().add(mPinOverlay);
+		mPinOverlay.addPoint(gp,null);
+
+		//LocationManagerで取得した位置をルート開始位置とします
+		mStartPos = gp;
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
+		//クリアメニューを追加
+		menu.removeItem(100);
+		menu.removeItem(MENUITEM_CLEAR);
+		menu.add(0, MENUITEM_CLEAR, 0, "クリア");
 		return true;
 	}
 }
